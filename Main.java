88fdedfeb37bd3479d6f883f99c741b2699e10b7
otherPOM <otherPOM@gmail.com>
2021-01-26 21:04:46 +0300

    Stage 1 done

--- Main.java
+++ Main.java
@@ -1,7 +1,43 @@
 package processor;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var an = scan.nextInt();
+        var am = scan.nextInt();
+        var a = new int[an][am];
+        for (int i = 0; i < an; i++) {
+            for (int j = 0; j < am; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        var bn = scan.nextInt();
+        var bm = scan.nextInt();
+        if (an != bn || am != bm) {
+            System.out.println("ERROR");
+            return;
+        }
+        var b = new int[bn][bm];
+        for (int i = 0; i < bn; i++) {
+            for (int j = 0; j < bm; j++) {
+                b[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int i1 = 0; i1 < a[i].length; i1++) {
+                a[i][i1] += b[i][i1];
+            }
+        }
+
+        Arrays.stream(a).forEach(row ->
+                System.out.println(Arrays.stream(row)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "))));
     }
 }
